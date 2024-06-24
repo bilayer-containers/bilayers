2fbe84bd354fb138a2127f279b838e3a9a5822d4
Rajavi Shah <shahraja@broadinstitute.org>
2024-06-24 11:31:31 -0400

    [mod] noxfile for building interface-image

--- noxfile.py
+++ noxfile.py
@@ -23,15 +23,31 @@ def build_algorithm(session):
         algorithm = session.posargs[0] 
     else:
         algorithm = 'threshold'
+    print("Building Algorithm Nox-File: ", algorithm)
     image_name = f'{algorithm}-image'
+    print("Image Name: ", image_name)
     dockerfile_path = f'src/Algorithms/{algorithm}/Dockerfile'
+    print("Dockerfile Path: ", dockerfile_path)
 
     # If Dockerfile doesn't exist for the specified algorithm
     if not os.path.exists(dockerfile_path):
+        print(f'Dockerfile for {algorithm} not found at {dockerfile_path}')
         session.error(f'Dockerfile for {algorithm} not found at {dockerfile_path}')
     session.run('docker', 'build', '-t', image_name, '-f', dockerfile_path, f'src/Algorithms/{algorithm}')
 
 @nox.session
-def build_gradio(session):
+def build_interface(session):
     """Build the Gradio docker Image"""
-    session.run('docker', 'build', '-t', 'gradio-image', '-f', 'src/build/Dockerfiles/Dockerfile', 'src/Build')
+    if len(session.posargs) > 0:
+        interface = session.posargs[0] 
+    else:
+        interface = 'gradio'
+    print("Building Interface Nox-File: ", interface)
+
+    image_name = f'{interface}-image'
+    print("Image Name: ", image_name)
+
+    dockerfile_path = f'src/Build/Dockerfiles/Dockerfile'
+    print("Dockerfile Path: ", dockerfile_path)
+
+    session.run('docker', 'build', '-t', image_name, '-f', dockerfile_path, 'src/Build')

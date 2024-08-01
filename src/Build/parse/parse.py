cd5bf01ba2fd8551804394524a25b51de07368f1
Rajavi Shah <shahraja@broadinstitute.org>
2024-08-01 18:01:44 -0400

    [Reorganised] Folder Structure and Template.j2 for Jupyter Notebook

--- src/Build/parse/parse.py
+++ src/Build/parse/parse.py
@@ -17,15 +17,19 @@ def main():
     
     # Exec function whole logic from threshold.py instead of just the function name
     exec_function = config.get('exec_function', {})
-    return sections, inputs, outputs, exec_function
+
+    folder_name = config.get('folder_name', None)
+
+    return sections, inputs, outputs, exec_function, folder_name
 
 if __name__ == "__main__":
     # sections, inputs, outputs, exec_function = main()
 
-    sections, inputs, outputs, exec_function = main()
+    sections, inputs, outputs, exec_function, folder_name = main()
     print(f"Sections: {sections}")
     print(f"Inputs: {inputs}")
     print(f"Outputs: {outputs}")
     print(f"Exec Function: {exec_function}")
+    print(f"Folder Name: {folder_name}")
 
     

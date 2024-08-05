d15dbe0420e5ab3f4136152bad72b8f06ca14633
Rajavi Shah <122663941+rajavishah@users.noreply.github.com>
2024-08-05 19:46:05 -0400

    V0.1 cellpose (#18)
    * [Add] Cellpose's Gradio generation via CLI Usage
    
    * [mod] Disable PR builds
    
    ---------
    
    Co-authored-by: Nodar Gogoberidze <gnodar01@gmail.com>

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,6 @@
 import nox
 import os
+import yaml
 
 @nox.session
 def run_parse(session):
@@ -16,24 +17,70 @@ def run_generate(session):
     session.run('python', 'generate.py')
     session.cd('../../..')
 
+# Prevoiusly, we had more basic nox-session for building the Algorithm docker image
+# @nox.session
+# def build_algorithm(session):
+#     """Build the Algorithm docker Image"""
+#     if len(session.posargs) > 0:
+#         algorithm = session.posargs[0] 
+#     else:
+#         algorithm = 'threshold'
+#     print("Building Algorithm Nox-File: ", algorithm)
+#     image_name = f'{algorithm}-image'
+#     print("Image Name: ", image_name)
+#     dockerfile_path = f'src/Algorithms/{algorithm}/Dockerfile'
+#     print("Dockerfile Path: ", dockerfile_path)
+
+#     # If Dockerfile doesn't exist for the specified algorithm
+#     if not os.path.exists(dockerfile_path):
+#         print(f'Dockerfile for {algorithm} not found at {dockerfile_path}')
+#         session.error(f'Dockerfile for {algorithm} not found at {dockerfile_path}')
+#     session.run('docker', 'build', '-t', image_name, '-f', dockerfile_path, f'src/Algorithms/{algorithm}')
+
+
 @nox.session
 def build_algorithm(session):
     """Build the Algorithm docker Image"""
     if len(session.posargs) > 0:
         algorithm = session.posargs[0] 
+        print("Algorithm Name in build_algorithm session: ", algorithm)
     else:
         algorithm = 'threshold'
+    
     print("Building Algorithm Nox-File: ", algorithm)
-    image_name = f'{algorithm}-image'
+    image_name = f'{algorithm}'
     print("Image Name: ", image_name)
     dockerfile_path = f'src/Algorithms/{algorithm}/Dockerfile'
+    config_file_path = f'src/Algorithms/{algorithm}/config.yaml'
+
     print("Dockerfile Path: ", dockerfile_path)
 
-    # If Dockerfile doesn't exist for the specified algorithm
-    if not os.path.exists(dockerfile_path):
+    if os.path.exists(dockerfile_path):
+        # If Dockerfile exists, build the image
+        session.run('docker', 'build', '-t', image_name, '-f', dockerfile_path, f'src/Algorithms/{algorithm}')
+    else:
+        # If Dockerfile doesn't exist, look into the config.yaml file
         print(f'Dockerfile for {algorithm} not found at {dockerfile_path}')
-        session.error(f'Dockerfile for {algorithm} not found at {dockerfile_path}')
-    session.run('docker', 'build', '-t', image_name, '-f', dockerfile_path, f'src/Algorithms/{algorithm}')
+        
+        if os.path.exists(config_file_path):
+            print(f'Checking config file at {config_file_path}')
+            with open(config_file_path, 'r') as file:
+                config = yaml.safe_load(file)
+            
+            # Extract the Docker image name from the config
+            org = config.get('docker-image', {}).get('org')
+            name = config.get('docker-image', {}).get('name')
+            tag = config.get('docker-image', {}).get('tag')
+            docker_image_name = f'{org}/{name}:{tag}' if org and name and tag else None
+            
+            if docker_image_name:
+                print(f'Found Docker image name in config: {docker_image_name}')
+                # Pull the Docker image from DockerHub
+                session.run('docker', 'pull', docker_image_name)
+            else:
+                session.error(f'Docker image name not found in {config_file_path}')
+        else:
+            session.error(f'Config file not found at {config_file_path}')
 
 @nox.session
 def build_interface(session):
@@ -50,4 +97,12 @@ def build_interface(session):
     dockerfile_path = f'src/Build/Dockerfiles/Dockerfile'
     print("Dockerfile Path: ", dockerfile_path)
 
-    session.run('docker', 'build', '-t', image_name, '-f', dockerfile_path, 'src/Build')
+    # Take the input from build_docker.sh file pass it whilst building the image
+    algorithm_name = session.posargs[1] 
+
+    session.run('docker', 'build', '--build-arg',  f'ALGORITHM_NAME={algorithm_name}', '-t', image_name, '-f', dockerfile_path, 'src/Build')
+
+@nox.session
+def install_gradio(session):
+    """Install Gradio"""
+    session.install('gradio')

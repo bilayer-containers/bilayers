a0a66079a8c06234bdd7834e6aa7cb40a2b5bc69
Rajavi Shah <shahraja@broadinstitute.org>
2024-09-23 14:52:19 -0400

    Linkml schema

--- src/Build/parse/parse.py
+++ src/Build/parse/parse.py
@@ -24,20 +24,20 @@ def main(config_path=None):
     # Exec function whole logic from threshold.py instead of just the function name
     exec_function = config.get('exec_function', {})
 
-    folder_name = config.get('folder_name', None)
+    algorithm_folder_name = config.get('algorithm_folder_name', None)
 
     citations = config.get('citations', [])
 
-    return parameters, display_only, results, exec_function, folder_name, citations
+    return parameters, display_only, results, exec_function, algorithm_folder_name, citations
 
 if __name__ == "__main__":
 
-    parameters, display_only, results, exec_function, folder_name, citations = main()
+    parameters, display_only, results, exec_function, algorithm_folder_name, citations = main()
     print(f"Parameters: {parameters}")
     print(f"Display Only: {display_only}")
     print(f"Results: {results}")
     print(f"Exec Function: {exec_function}")
-    print(f"Folder Name: {folder_name}")
+    print(f"Folder Name: {algorithm_folder_name}")
     print(f"Citations: {citations}")
 
     

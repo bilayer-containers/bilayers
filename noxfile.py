aeea8eaf5a90cb545e9da2d90c1943b7e5d0214c
Rajavi Shah <shahraja@broadinstitute.org>
2024-10-29 09:15:16 -0400

    renamed; spec file for instanseg

--- noxfile.py
+++ noxfile.py
@@ -22,67 +22,71 @@ def run_generate(session):
 def build_algorithm(session):
     """Build the Algorithm docker Image"""
     if len(session.posargs) > 0:
-        algorithm = session.posargs[0] 
+        algorithm = session.posargs[0]
         print("Algorithm Name in build_algorithm session: ", algorithm)
     else:
-        algorithm = 'threshold'
+        algorithm = 'threshold_inference'
     
     print("Building Algorithm Nox-File: ", algorithm)
     image_name = f'{algorithm}'
     print("Image Name: ", image_name)
-    dockerfile_path = f'src/Algorithms/{algorithm}/Dockerfile'
-    config_file_path = f'src/Algorithms/{algorithm}/config.yaml'
+    dockerfile_path = f'src/algorithms/{algorithm}/Dockerfile'
+    config_file_path = f'src/algorithms/{algorithm}/config.yaml'
 
-    print("Dockerfile Path: ", dockerfile_path)
-
-    if os.path.exists(dockerfile_path):
-        # If Dockerfile exists, build the image
-        session.run('docker', 'build', '-t', image_name, '-f', dockerfile_path, f'src/Algorithms/{algorithm}')
-    else:
-        # If Dockerfile doesn't exist, look into the config.yaml file
-        print(f'Dockerfile for {algorithm} not found at {dockerfile_path}')
+    # Start by checking the config file for DockerHub image details
+    if os.path.exists(config_file_path):
+        print(f'Checking config file at {config_file_path}')
+        with open(config_file_path, 'r') as file:
+            config = yaml.safe_load(file)
         
-        if os.path.exists(config_file_path):
-            print(f'Checking config file at {config_file_path}')
-            with open(config_file_path, 'r') as file:
-                config = yaml.safe_load(file)
-            
-            # Extract the Docker image name from the config
-            org = config.get('docker_image', {}).get('org')
-            name = config.get('docker_image', {}).get('name')
-            tag = config.get('docker_image', {}).get('tag')
-            docker_image_name = f'{org}/{name}:{tag}' if org and name and tag else None
-            algorithm_folder_name = config.get('algorithm_folder_name', None)
+        org = config.get('docker_image', {}).get('org')
+        name = config.get('docker_image', {}).get('name')
+        tag = config.get('docker_image', {}).get('tag')
+        docker_image_name = f'{org}/{name}:{tag}' if org and name and tag else None
+        algorithm_folder_name = config.get('algorithm_folder_name', None)
 
+        # Save the algorithm folder name in a file
+        with open('/tmp/algorithm_folder_name.txt', 'w') as file:
+            file.write(algorithm_folder_name)
+
+        # Attempt to pull the image from DockerHub
+        try:
+            print(f'Trying to pull the Docker image: {docker_image_name}')
+            session.run('docker', 'pull', docker_image_name)
+            print(f'Successfully pulled Docker image from DockerHub: {docker_image_name}')
             # Save the Docker image name in a file
             with open('/tmp/docker_image_name.txt', 'w') as file:
                 file.write(docker_image_name)
-
-            with open('/tmp/algorithm_folder_name.txt', 'w') as file:
-                file.write(algorithm_folder_name)
-                
-            if docker_image_name:
-                print(f'Found Docker image name in config: {docker_image_name}')
-                # Pull the Docker image from DockerHub
-                session.run('docker', 'pull', docker_image_name)
+        except Exception as e:
+            print(f'Failed to pull Docker image from DockerHub. Error: {e}')
+            # Proceed to build from Dockerfile if pull fails
+            if os.path.exists(dockerfile_path):
+                print("Pull failed; attempting to build locally from Dockerfile.")
+                session.run('docker', 'build', '-t', image_name, '-f', dockerfile_path, f'src/algorithms/{algorithm}')
+                # Save the locally built Docker image name in a file
+                with open('/tmp/docker_image_name.txt', 'w') as file:
+                    file.write(image_name)
             else:
-                session.error(f'Docker image name not found in {config_file_path}')
-        else:
-            session.error(f'Config file not found at {config_file_path}')
+                session.error(f'Neither Docker image on DockerHub nor Dockerfile found for {algorithm}')
+
+    else:
+        # If the config file does not exist, report an error
+        session.error(f'Config file not found at {config_file_path}')
 
 @nox.session
 def build_interface(session):
     """Build the Gradio docker Image"""
     if len(session.posargs) > 0:
-        interface = session.posargs[0] 
+        interface = session.posargs[0]
+        algorithm = session.posargs[1]
     else:
         interface = 'gradio'
     print("Building Interface Nox-File: ", interface)
 
-    image_name = f'{interface}-image'
+    image_name = f'{algorithm}_{interface}_image'
     print("Image Name: ", image_name)
 
-    dockerfile_path = f'src/build/Dockerfiles/{interface.capitalize()}.Dockerfile'
+    dockerfile_path = f'src/build/dockerfiles/{interface.capitalize()}.Dockerfile'
     print("Dockerfile Path: ", dockerfile_path)
 
     # Read the Docker image name from the file

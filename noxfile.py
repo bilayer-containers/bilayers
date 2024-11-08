44e30b0108e2e665182b5d3280a703c1ca7544b8
Rajavi Shah <shahraja@broadinstitute.org>
2024-11-08 13:11:53 -0500

    [see] workflow with amd64

--- noxfile.py
+++ noxfile.py
@@ -97,7 +97,7 @@ def build_interface(session):
         algorithm_folder_name = file.read().strip()
         
     if interface == 'gradio':
-        session.run('docker', 'build', '-f', 'Gradio.Dockerfile', '--build-arg',  f'BASE_IMAGE={base_image}', '--build-arg',  f'FOLDER_NAME={algorithm_folder_name}', '-t', image_name, '-f', dockerfile_path, 'src/build')
+        session.run('docker', 'build', '--platform', 'linux/amd64', '-f', 'Gradio.Dockerfile', '--build-arg',  f'BASE_IMAGE={base_image}', '--build-arg',  f'FOLDER_NAME={algorithm_folder_name}', '-t', image_name, '-f', dockerfile_path, 'src/build')
     elif interface == 'jupyter':
         session.run('docker', 'build', '-f', 'Jupyter.Dockerfile', '--build-arg',  f'BASE_IMAGE={base_image}', '--build-arg',  f'FOLDER_NAME={algorithm_folder_name}', '-t', image_name, '-f', dockerfile_path, 'src/build')
 

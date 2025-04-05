27ce8de917029989215cc46d37abac367a9e927d
Rajavi Shah <shahraja@broadinstitute.org>
2025-04-05 19:17:05 -0400

    linkml schema rule respective changes for modifying citations

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -8,11 +8,11 @@ ALGORITHM_NAMES=("cellpose_inference" "classical_segmentation" "instanseg_infere
 echo "Validating all config files against the schema $SCHEMA_FILE"
 
 for ALGORITHM in "${ALGORITHM_NAMES[@]}"; do
-    CONFIG_DIR="../src/algorithms/${ALGORITHM}"
+    CONFIG_DIR="../src/bilayers/algorithms/${ALGORITHM}"
     echo "Validating all config files in $CONFIG_DIR"
     for config in "$CONFIG_DIR"/*.yaml; do
         echo "Validating $config..."
-        linkml validate --schema "$SCHEMA_FILE" --target-class Container "$config" || {
+        linkml validate --schema "$SCHEMA_FILE" --target-class SpecContainer "$config" || {
             echo "Validation failed for $config"
             exit 1
         }

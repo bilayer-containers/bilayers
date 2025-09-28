694793eae9aba0b7431b1fcf2ec82e6b46c77f86
Rajavi Shah <shahraja@broadinstitute.org>
2025-09-27 20:27:03 -0400

    [del] commented code

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -54,29 +54,3 @@ for ALGO in "${ALGORITHM_NAMES[@]}"; do
     fi
   done
 done
-
-# # Build the Docker images for each algorithm and interface
-# for ALGORITHM_NAME in "${ALGORITHM_NAMES[@]}";
-#     do
-#     for INTERFACE_NAME in "${INTERFACE_NAMES[@]}";
-#         do
-#         echo "Processing Algorithm: $ALGORITHM_NAME, Interface: $INTERFACE_NAME"
-
-#         # Running the parse script by giving actual config_path
-#         CONFIG_PATH="../../../../src/bilayers/algorithms/${ALGORITHM_NAME}/config.yaml"
-#         nox -s run_parse -- $CONFIG_PATH
-
-#         # Running the generate file
-#         CONFIG_PATH="../../../../src/bilayers/algorithms/${ALGORITHM_NAME}/config.yaml"
-#         nox -s run_generate -- $CONFIG_PATH
-
-#         # Building the Algorithm Docker image
-#         nox -s build_algorithm -- $ALGORITHM_NAME
-
-#         # Building the Interface Docker image
-#         nox -s build_interface -- $INTERFACE_NAME $ALGORITHM_NAME
-
-#         # Installing the Graadio interface
-#         nox -s install_gradio
-#     done
-# done

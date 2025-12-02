a4aa98bde55750d33ec3fdbfe4401b80b67f885e
bethac07 <bcimini@broadinstitute.org>
2025-12-02 09:53:37 -0500

    Building version

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -61,6 +61,8 @@ for ALGO in "${ALGORITHM_NAMES[@]}"; do
 
     if [[ "$IFACE" == "gradio" ]]; then
       nox -s install_gradio
+    elif [[ "$IFACE" == "streamlit" ]]; then
+      nox -s install_streamlit
     fi
   done
 done

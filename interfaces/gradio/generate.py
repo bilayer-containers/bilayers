83109f726a2c11c6b6cb9fe2101e182c38e90a76
Rajavi Shah <shahraja@broadinstitute.org>
2026-02-18 03:58:50 -0500

    [add] handle numeric param w default None via split

--- interfaces/gradio/generate.py
+++ interfaces/gradio/generate.py
@@ -59,10 +59,43 @@ def generate_gradio_app(
 
     title, full_description = generate_top_level_text(CITATION, citations, output_html=False)
 
+    split_parameters = {}
+    parameters_flat = {}
+
+    for paramkey, paramvalue in parameters.items():
+        assert "type" in paramvalue and "default" in paramvalue and "label" in paramvalue
+        if paramvalue["type"] in ["float", "integer"] and str(paramvalue["default"]) in ["None", "False"]:
+            # "Set a value?" toggle
+            bool_param = dict(paramvalue)
+            bool_param["name"] = f"set_{paramkey}"
+            bool_param["type"] = "checkbox"
+            bool_param["default"] = False
+            bool_param["cli_tag"] = ""
+            bool_param["optional"] = True
+            newlabel = paramvalue["label"][0].lower() + paramvalue["label"][1:]
+            bool_param["label"] = f"Set a value for {newlabel}?"
+            bool_param["description"] = paramvalue.get("description", "")
+
+            # Actual number input (hidden unless checkbox is ticked)
+            num_param = dict(paramvalue)
+            num_param["name"] = paramkey
+            num_param["default"] = 0
+            num_param["optional"] = True
+
+            split_parameters[paramkey] = {
+                f"set_{paramkey}": bool_param,
+                paramkey: num_param,
+            }
+            parameters_flat[f"set_{paramkey}"] = bool_param
+            parameters_flat[paramkey] = num_param
+        else:
+            parameters_flat[paramkey] = paramvalue
+
     gradio_app_code: str = template.render(
         inputs=inputs,
         outputs=outputs,
-        parameters=parameters,
+        parameters=parameters_flat,
+        split_parameters=split_parameters,
         display_only=display_only,
         exec_function=exec_function,
         title=title,

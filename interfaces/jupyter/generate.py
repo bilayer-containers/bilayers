2d06f05ddb549b4d296e43e2408b4359164bfffb
Rajavi Shah <shahraja@broadinstitute.org>
2026-03-11 20:19:55 -0400

    [restructure p1] Update interfaces to import schema types from bilayers_schema

--- interfaces/jupyter/generate.py
+++ interfaces/jupyter/generate.py
@@ -6,7 +6,7 @@ import nbformat as nbf
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from bilayers import project_path
-from bilayers.parse import Citations, Input, Output, Parameter, ExecFunction, DockerImage
+from bilayers_schema import Citations, Input, Output, Parameter, ExecFunction, DockerImage
 from bilayers.generate import generate_top_level_text
 
 CITATION: Citations = {

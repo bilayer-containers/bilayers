7c91142cb8ef08529b85c8afcae006418f840dd3
Rajavi Shah <shahraja@broadinstitute.org>
2026-05-05 09:07:32 -0400

    [phase4] extract schema into bilayers-schema package

--- src/bilayers/__init__.py
+++ src/bilayers/__init__.py
@@ -3,7 +3,7 @@ from ._blpath import package_path
 from . import parse, generate
 from .cli_generator import generate_cli_command
 from .cli import cli
-from .schema import schema, print_schema
+from bilayers_schema import schema, print_schema
 
 
 __all__ = ["package_path", "parse", "generate", "generate_cli_command", "cli", "schema", "print_schema"]
